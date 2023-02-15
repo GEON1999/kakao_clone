2f52c3ae14885f9f36b14045d6b53d06dcb7f7d1
DESKTOP-SHB77O2\phgst <phgst12@gmail.com>
2023-02-15 13:42:19 +0900

    deploy

--- src/index.js
+++ src/index.js
@@ -70,14 +70,14 @@ a {
 }
 `;
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <GlobalStyle />
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
